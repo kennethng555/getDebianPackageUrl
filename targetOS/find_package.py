4d8c0efe6bbbc137f25f82841c476f2ed4cf0933
kennethng555 <75182837+kennethng555@users.noreply.github.com>
2026-03-06 14:46:10 -0500

    Update find_package.py
    speedup find_package

--- targetOS/find_package.py
+++ targetOS/find_package.py
@@ -1,18 +1,26 @@
 import sys
 
-def load_package_list(file_path):
+def extract_package_name(url):
     """
-    Loads a list of package names from a file (one per line).
-    
-    Args:
-    - file_path: Path to the text file containing the package names.
-    
-    Returns:
-    - A list of package names.
+    Extract package name from a .deb URL.
+    Example:
+    http://.../libssl1.1_1.1.1f-1ubuntu2_amd64.deb
+    -> libssl1.1_1.1.1f-1ubuntu2_amd64.deb
     """
+    return url.split('/')[-1]
+
+def load_urls(file_path):
+    urls = {}
     with open(file_path, 'r') as f:
-        package_list = [line.strip() for line in f.readlines()]
-    return package_list
+        for line in f.readlines():
+            url = line.strip()
+            package_name = extract_package_name(url)
+            urls[package_name] = url
+    return urls
+
+def load_package_list(file_path):
+    with open(file_path, 'r') as f:
+        return [line.strip() for line in f.readlines()]
 
 if len(sys.argv) < 3:
     print("Usage: ubuntu_scraper.py <package_list_file> <output_file> <full_list_file>\n\n")
@@ -30,18 +38,18 @@ f = open(output_file, 'w')
 
 # Load the package names from the file
 package_list = load_package_list(package_list_file)
-url_list = load_package_list(full_list_file)
+url_list = load_urls(full_list_file)
 found = [0] * len(package_list)
 
 not_found = 0
 for idx, package in enumerate(package_list):
-  for url in url_list:
-    if package in url:
-        f.write(f'{url}\n')
+    if package in url_list:
+        f.write(f'{url_list[package]}\n')
+        print(f'Found package: {package} -> {url_list[package]}')
         found[idx] = 1
-  if found[idx] == 0:
-    print(f'Package not found: {package}')
-    not_found = not_found + 1
+    else:
+        print(f'Package not found: {package}')
+        not_found += 1
 
 print(f'Total packages not found:{not_found}')
 print(f'Total packages: {len(package_list)}')
